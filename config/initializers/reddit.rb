0aa25790fa659acc83a21ac9bfb568fa0e0e8634
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-01 14:15:51 +0100

    added examples for store remote

--- config/initializers/reddit.rb
+++ config/initializers/reddit.rb
@@ -1,7 +1,9 @@
 # You need to set up an environment variable with RSS_FEED
+# export RSS_FEED="http://pipes.yahoo.com/pipes/pipe.run?_id=4a3af06dcba612424a858ea79dd263db&_render=rss&subreddit=funny&threshold=100"
 $RSS_FEED =  ENV['RSS_FEED']
 
 # Should only the remote image link be stored?
+# export STORE_REMOTE=true
 $STORE_REMOTE =  ENV['STORE_REMOTE']
 if $STORE_REMOTE
   $STORE_REMOTE = true
