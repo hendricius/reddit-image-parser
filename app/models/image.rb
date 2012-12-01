b0e5615d07058cc202e51d83a271289c61856992
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-01 10:45:12 +0100

    added image parser functions

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,24 @@
 class Image < ActiveRecord::Base
   attr_accessible :image
   mount_uploader :image, ImageUploader
+
+  def get_feed
+    Feedzirra::Feed.fetch_and_parse($REDDIT[:rss_feed])
+  end
+
+  def parse_feed
+    get_feed.entries.each do |entry|
+      # do not parse those that do not point to an image
+      next if !entry.url.include?(".jpg")
+      save_image(entry)
+    end
+  end
+
+  def save_image(entry)
+    puts entry
+    # entry.url contains the link to the feed.
+   i = Image.new
+   i.remote_image_url = entry.url
+   i.save
+  end
 end
