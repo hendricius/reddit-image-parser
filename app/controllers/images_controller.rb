3de9e2cba0e1f1d6fcb646cce88edcf4c2cafe07
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-01 11:37:33 +0100

    redirect to last image by default

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,4 +8,9 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find_by_id(params[:id])
   end
+
+  def latest
+    @image = Image.last
+    redirect_to @image
+  end
 end
