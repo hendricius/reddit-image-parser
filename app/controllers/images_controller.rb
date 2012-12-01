b0c5815c365410a948f8f56dbb788351efcaa584
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-01 11:04:06 +0100

    added show action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,11 @@
 class ImagesController < ApplicationController
   respond_to :html
+
   def index
     @images = Image.all
+  end
+
+  def show
+    @image = Image.find_by_id(params[:id])
   end
 end
