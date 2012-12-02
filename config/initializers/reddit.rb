a317923baa1a9665ed1e8e9505352b65adfa90d1
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-02 17:36:45 +0100

    add new environment variables

--- config/initializers/reddit.rb
+++ config/initializers/reddit.rb
@@ -1,6 +1,11 @@
 # You need to set up an environment variable with RSS_FEED
 # export RSS_FEED="http://pipes.yahoo.com/pipes/pipe.run?_id=4a3af06dcba612424a858ea79dd263db&_render=rss&subreddit=funny&threshold=100"
-$RSS_FEED =  ENV['RSS_FEED']
+$REDDIT_RSS =  ENV['REDDIT_RSS']
+
+# For using JSON. If JSON is detected, it has higher importance than RSS. Set
+# the environment variables as well.
+$REDDIT_JSON = ENV['REDDIT_JSON']
+$REDDIT_THRESHOLD = ENV['REDDIT_THRESHOLD']
 
 # Should only the remote image link be stored?
 # export STORE_REMOTE=true
