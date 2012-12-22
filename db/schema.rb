4de668524a082a96b932b187023f81a602a3be19
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-22 17:39:08 +0100

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201125948) do
+ActiveRecord::Schema.define(:version => 20121222163713) do
 
   create_table "images", :force => true do |t|
     t.datetime "created_at",    :null => false
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(:version => 20121201125948) do
     t.string   "title"
     t.string   "external_id"
     t.string   "external_link"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username",         :null => false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
