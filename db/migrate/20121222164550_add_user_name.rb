818e0078147e393cf89e20d6eebb09eaa61a47e3
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-22 17:46:45 +0100

    add username

---
+++ db/migrate/20121222164550_add_user_name.rb
@@ -0,0 +1,5 @@
+class AddUserName < ActiveRecord::Migration
+  def change
+    add_column :users, :user_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121222163940) do
+ActiveRecord::Schema.define(:version => 20121222164550) do
 
   create_table "images", :force => true do |t|
     t.datetime "created_at",    :null => false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20121222163940) do
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
     t.boolean  "external"
+    t.string   "user_name"
   end
 
 end
