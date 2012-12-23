a0b6fc6eb9eb0ed23b5e993f75f80a79db6e382d
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-23 19:13:21 +0100

    removed non public routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RedditRss::Application.routes.draw do
   root :to => 'images#latest'
-  resources :users do
-    resources :images, :shallow => true
+  resources :users, only: [] do
+    resources :images, :shallow => true, only: [:index, :show]
   end
   get 'users/:user_id/images/:image_id' => 'images#user_image', as: "user_image_collection"
 end
