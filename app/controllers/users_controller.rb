029553bf421914db208ebefae68d21849f24837e
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-24 11:54:17 +0100

    add user reg routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   respond_to :html
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 RedditRss::Application.routes.draw do
   root :to => 'images#latest'
-  resources :users, only: [] do
+  resources :users, only: [:new, :create] do
     resources :images, :shallow => true, only: [:index, :show]
   end
   get 'users/:user_id/images/:image_id' => 'images#user_image', as: "user_image_collection"
   get "top_users" => "images#top_users", as: "top_users"
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  resources :sessions
 end
