6a7a7f2400bc8e2cd551499e54d423586dce1ee7
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-24 14:24:13 +0100

    add remember me token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   respond_to :html
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
+    user = login(params[:email], params[:password], true)
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121222172835) do
+ActiveRecord::Schema.define(:version => 20121224132129) do
 
   create_table "images", :force => true do |t|
     t.datetime "created_at",    :null => false
@@ -25,13 +25,17 @@ ActiveRecord::Schema.define(:version => 20121222172835) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",         :null => false
+    t.string   "username",                     :null => false
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
     t.boolean  "external"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
+
+  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
 
 end
