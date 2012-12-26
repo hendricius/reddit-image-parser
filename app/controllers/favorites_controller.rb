d60227bb305763e1474079d5670b98d1b4989cad
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-27 00:06:55 +0100

    added localization for time

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   respond_to :html
 
   def index
-    @favorites = current_user.favorites
+    @favorites = current_user.favorites.order("created_at desc")
   end
 
 end
