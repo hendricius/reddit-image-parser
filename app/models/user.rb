ad57df1ec89793f7bc3bc0ed29cbb96db20b2e52
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2012-12-28 02:02:16 +0100

    fix bug with external

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  attr_accessible :email, :password, :password_confirmation, :username
+  attr_accessible :email, :password, :password_confirmation, :username, :external
   attr_accessor :total_images
 
   has_many :images
